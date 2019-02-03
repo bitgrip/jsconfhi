9e8c35b1bd3bc679327ad197fa7e8057febfcc7c
Joerg Patzer <joerg.patzer@bitgrip.de>
2019-02-03 23:47:47 +0100

    add .nojekyll file for Github Page to bypassing jekyll processing

---
+++ docs/_next/static/5iXx4CiXvE6_zDard~oOK/pages/content/message.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{209:function(n,e,o){__NEXT_REGISTER_PAGE("/content/message",function(){return n.exports=o(76),{page:n.exports.default}})}},[[209,1,0]]]);
